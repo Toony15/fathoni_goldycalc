e218dec370a47b7f0362c0b877f776962f7450ba
Muhammad Ramadhan Fathoni <mramadhanfathoni@gmail.com>
2025-04-07 18:14:25 +0700

    Top App Bar Progression

--- app/src/main/java/com/toni/goldycalc/MainActivity.kt
+++ app/src/main/java/com/toni/goldycalc/MainActivity.kt
@@ -6,10 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.toni.goldycalc.ui.theme.GoldyCalcTheme
 
@@ -18,16 +23,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            GoldyCalcTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
         }
     }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen () {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        },
+
+
+    ) { innerPadding ->
+        ScreenContent(Modifier.padding(innerPadding))
+
+    }
+}
+@Composable
+fun ScreenContent ( modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello Android!",
+        modifier = modifier
+    )
 }
 
 @Composable
@@ -42,6 +69,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GoldyCalcTheme {
-        Greeting("Android")
+       MainScreen()
     }
 }
